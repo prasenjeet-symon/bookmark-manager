4a69d9e647106a97cef2454ba62976d0c91b15ee
prasenjeet-symon <prasenjeetsymon@outlook.com>
2024-02-03 20:29:34 +0530

    Feature: Enhance NetworkApi with Sign-in Functionality
    This commit extends the NetworkApi class to include a signin() method for authenticating users with email and password.
    
    NetworkApi Class:
    - Adds the signin() method to handle user authentication by making a POST request to the sign-in endpoint.
    - Utilizes the HttpManager class for handling HTTP requests with proper authorization.
    
    These enhancements aim to provide a comprehensive API interaction layer, facilitating user sign-in functionality within the application.

--- packages/client/bmclient/src/datasource/network.api.ts
+++ packages/client/bmclient/src/datasource/network.api.ts
@@ -3,6 +3,7 @@ import { HttpManager } from "./http/http.manager";
 export class NetworkApi {
   private baseUrl = "http://localhost:8081/server";
   private signUp = `${this.baseUrl}/auth/signup`;
+  private signIn = `${this.baseUrl}/auth/login`;
 
   /**
    *
@@ -20,4 +21,25 @@ export class NetworkApi {
       body: JSON.stringify(reqBody),
     });
   }
+
+  /**
+   *
+   * Sign in with email and password
+   */
+  public signin(email: string, password: string) {
+    const reqBody = {
+      email: email,
+      password: password,
+    };
+
+    return HttpManager.request(this.signIn, {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+    });
+  }
+
+  /**
+   * 
+   * 
+   */
 }
